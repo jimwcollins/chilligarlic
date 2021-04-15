abaa457a5748d62e6c079457667fc90f3f04dd49
James Collins <jimwcollins@gmail.com>
2021-04-15 17:40:37 +0100

    adds mobile search, needs more styling

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -10,107 +10,147 @@ class Header extends HTMLElement {
     this.isHome = this.getAttribute('home');
 
     this.innerHTML = /*html*/ `
-      <header class="header grid" data-theme=${
+      <header class="header-container" data-theme=${
         this.isHome ? 'header-home' : 'header-main'
       }>
 
-      <!-- Search -->
-      <form
-        action="searchResults.html"
-        class="header__search__form"
-        method="GET"
-        id="search"
-      >
-        <input
-          type="text"
-          class="header__search__input"
-          name="search"
-          autocomplete="off"
-          placeholder="Search recipes"
-        />
-        <button class="header__search__button" type="button">
-          <svg class="header__search__icon">
-            <use
-              xlink:href="img/Chilli_Icons_Sprite.svg#icon-magnifying-glass"
-            ></use>
+      <div class="header grid">
+        <!-- Search -->
+        <form
+          action="searchResults.html"
+          class="header__search__form"
+          method="GET"
+          id="search"
+        >
+          <input
+            type="text"
+            class="header__search__input"
+            name="search"
+            autocomplete="off"
+            placeholder="Search recipes"
+          />
+          <button class="header__search__button" type="button">
+            <svg class="header__search__icon">
+              <use
+                xlink:href="img/Chilli_Icons_Sprite.svg#icon-magnifying-glass"
+              ></use>
+            </svg>
+          </button>
+        </form>
+
+        <figure class="header__icon header__search--mob">
+          <svg class="header__icon__svg">
+            <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-magnifying-glass"></use>
           </svg>
-        </button>
-      </form>
+        </figure>
 
-      <!-- Logo -->
-      <a href="/" class="header__logo"><img
-          src="img/Logo/Chill_Logo_Red.png"
-          alt=""
-          class="header__logo__img"
-      /></a>
 
-      <!-- Nav elements -->
-      <nav class="header__nav">
-        <!-- Faves dropdown menu-->
-        <div class="menu">
-          <!-- Fave Icon -->
-          <figure class="header__icon">
-            <svg class="header__icon__svg">
-              <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-heart"></use>
-            </svg>
-          </figure>
+        <!-- Logo -->
+        <a href="/" class="header__logo"><img
+            src="img/Logo/Chill_Logo_Red.png"
+            alt=""
+            class="header__logo__img"
+        /></a>
 
-          <!-- Fave Dropdown -->
-          <div class="menu__panel">
-            <div class="menu__dropdown" id="menu__faves">
-              <div class="menu__dropdown__header">
-                <p class="menu__dropdown__header__text">My Favourites</p>
-                <svg class="menu__dropdown__header__icon" id="faves__clear">
-                  <use
-                    xlink:href="img/Chilli_Icons_Sprite.svg#icon-circle-with-cross"
-                  ></use>
-                </svg>
+        <!-- Nav elements -->
+        <nav class="header__nav">
+          <!-- Faves dropdown menu-->
+          <div class="menu">
+            <!-- Fave Icon -->
+            <figure class="header__icon">
+              <svg class="header__icon__svg">
+                <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-heart"></use>
+              </svg>
+            </figure>
+
+            <!-- Fave Dropdown -->
+            <div class="menu__panel">
+              <div class="menu__dropdown" id="menu__faves">
+                <div class="menu__dropdown__header">
+                  <p class="menu__dropdown__header__text">My Favourites</p>
+                  <svg class="menu__dropdown__header__icon" id="faves__clear">
+                    <use
+                      xlink:href="img/Chilli_Icons_Sprite.svg#icon-circle-with-cross"
+                    ></use>
+                  </svg>
+                </div>
+
+                <div class="menu__placeholder" id="faves__placeholder">
+                  <p class="menu__placeholder__text">
+                    Love a recipe? This could be its new home!
+                  </p>
+                </div>
+
+                <ul class="scrollableDropdown" id="faves__list"></ul>
               </div>
-
-              <div class="menu__placeholder" id="faves__placeholder">
-                <p class="menu__placeholder__text">
-                  Love a recipe? This could be its new home!
-                </p>
-              </div>
-
-              <ul class="scrollableDropdown" id="faves__list"></ul>
             </div>
           </div>
-        </div>
 
-        <!-- Shopping list dropdown menu-->
-        <div class="menu">
-          <!-- Shopping List icon-->
-          <figure class="header__icon">
-            <svg class="header__icon__svg">
-              <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-list2"></use>
-            </svg>
-          </figure>
+          <!-- Shopping list dropdown menu-->
+          <div class="menu">
+            <!-- Shopping List icon-->
+            <figure class="header__icon">
+              <svg class="header__icon__svg">
+                <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-list2"></use>
+              </svg>
+            </figure>
 
-          <!-- Shopping List dropdown-->
-          <div class="menu__panel">
-            <div class="menu__dropdown" id="menu__shopList">
-              <div class="menu__dropdown__header">
-                <p class="menu__dropdown__header__text">My Shopping List</p>
-                <svg class="menu__dropdown__header__icon" id="shopList__clear">
-                  <use
-                    xlink:href="img/Chilli_Icons_Sprite.svg#icon-circle-with-cross"
-                  ></use>
-                </svg>
+            <!-- Shopping List dropdown-->
+            <div class="menu__panel">
+              <div class="menu__dropdown" id="menu__shopList">
+                <div class="menu__dropdown__header">
+                  <p class="menu__dropdown__header__text">My Shopping List</p>
+                  <svg class="menu__dropdown__header__icon" id="shopList__clear">
+                    <use
+                      xlink:href="img/Chilli_Icons_Sprite.svg#icon-circle-with-cross"
+                    ></use>
+                  </svg>
+                </div>
+
+                <div class="menu__placeholder" id="shopList__placeholder">
+                  <p class="menu__placeholder__text">
+                    Make your shopping list happy by filling it with lovely
+                    ingredients.
+                  </p>
+                </div>
+
+                <ul class="scrollableDropdown" id="shopList__list"></ul>
               </div>
-
-              <div class="menu__placeholder" id="shopList__placeholder">
-                <p class="menu__placeholder__text">
-                  Make your shopping list happy by filling it with lovely
-                  ingredients.
-                </p>
-              </div>
-
-              <ul class="scrollableDropdown" id="shopList__list"></ul>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
+
+      <div class="header__mobileSearch grid">
+        <!-- Search -->
+        <form
+          action="searchResults.html"
+          class="header__mobileSearch__form"
+          method="GET"
+          id="search"
+        >
+          <input
+            type="text"
+            class="header__mobileSearch__input"
+            name="search"
+            autocomplete="off"
+            placeholder="Search recipes"
+          />
+
+          <div class="header__mobileSearch__controls">          
+            <figure class="header__icon header__search--mob">
+              <svg class="header__icon__svg">
+                <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-magnifying-glass"></use>
+              </svg>
+            </figure>
+            <button class="header__icon" type="button">
+              <svg class="header__search__icon">
+                <use xlink:href="img/Chilli_Icons_Sprite.svg#icon-magnifying-glass"></use>
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
     </header>
     `;
 
@@ -184,6 +224,16 @@ class Header extends HTMLElement {
 
       headerObserverMain.observe(heading);
     }
+
+    // Handle mobile search
+
+    document
+      .querySelector('.header__search--mob')
+      .addEventListener('click', (event) => {
+        console.log('Reveal mobile search');
+        const mobSearch = document.querySelector('.header__mobileSearch');
+        mobSearch.classList.toggle('header__mobileSearch--active');
+      });
   }
 }
 
